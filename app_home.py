73bee7fc93fac9c1ef65f6fac4f2a0b971958cd5
CongSeung <leem1121@gmail.com>
2022-06-08 17:51:41 +0900

    Update app_home.py

--- app_home.py
+++ app_home.py
@@ -8,9 +8,6 @@ import streamlit as st
 import base64
 import webbrowser
 
-
-from app_ml import run_ml
-
 def run_home():
     st.subheader('신용카드 거래 사기 여부 판단해드립니다.')
     st.text('좌측 메뉴를 통해 데이터를 탐색해보고, 카드 결제건에 대해 사기 여부를 알 수 있습니다.')
@@ -21,30 +18,6 @@ def run_home():
     st.text('금융민원관련 상담전화, 국번없이 1332')
     if st.button('금융감독원 바로가기'):
         webbrowser.open_new_tab(url)
-
-    #### 이미지 배경으로 만들기
-    # @st.cache(allow_output_mutation=True)
-    # def get_base64_of_bin_file(bin_file):
-    #     with open(bin_file, 'rb') as f:
-    #         data = f.read()
-    #     return base64.b64encode(data).decode()
-
-    # def set_png_as_page_bg(png_file):
-    #     bin_str = get_base64_of_bin_file(png_file)
-    #     page_bg_img = '''
-    #     <style>
-    #     body {
-    #     background-image: url("data:image/png;base64,%s");
-    #     background-size: cover;
-    #     }
-    #     </style>
-    #     ''' % bin_str
-        
-    #     st.markdown(page_bg_img, unsafe_allow_html=True)
-    #     return
-
-    # set_png_as_page_bg('data/streamlit-app-2022-05-27-15-05-59.gif')
-
 
     ### gif 파일 넣기
     """### gif from local file"""
@@ -58,9 +31,3 @@ def run_home():
         unsafe_allow_html=True,
     )
 
-
-    #### 영상넣기
-    # st.text('영상 예시')
-    # video_file = open('data/streamlit-app-2022-05-27-15-05-59.webm', 'rb')
-    # st.video(video_file)
-        
